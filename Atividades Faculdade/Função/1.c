917e68465e8c3d2189364022935315c7b6478274
Artur <81937745+Dubai782@users.noreply.github.com>
2021-09-30 22:38:02 -0300

    Add files via upload

---
+++ Atividades Faculdade/Função/1.c
@@ -0,0 +1,18 @@
+/*1- Explique a diferença entre função e procedimento, use exemplos.
+Então um procedimento é um algoritmo que será executado(é quase igual a uma função, mas não precisa necessariamente ter retorno algum), enquanto que a função é um algoritmo que será executado e produzirá um resultado final concreto que poderá ser usado por outro algoritmo.*/
+
+****************************************************************
+ //Função//
+
+Operador nome_funcao(parametros)
+{
+    Condição;
+    Return = nome_variavel;
+}
+
+//Procedimento//
+
+Operador nome_procedimento()
+{
+    Método;
+}
